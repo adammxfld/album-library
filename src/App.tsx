d1474b2cff309b7d1fa2a8521c6df3bc132eddc6
Adam Maxfield <adammaxfield@gmail.com>
2026-09-01 14:44:52 -0700

    Add basic top tier filter

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import AlbumList from './components/AlbumList.js'
 import { albums } from "./albums_data.ts";
 
 function App() {
+  const tierOneAlbums = albums.filter((album) => album.tier === 1)
+
   return (
       <section id="center">
-        <AlbumList albums={albums} />
+        <AlbumList albums={tierOneAlbums} />
       </section>
   )
 }
