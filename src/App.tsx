a4ba8ea1bb8424af48fa1116d388f3f0c7af8f7d
Adam Maxfield <adammaxfield@gmail.com>
2026-09-10 14:44:24 -0700

    Adds design for the main album list component.

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,34 @@ import AlbumList from './components/AlbumList.js'
 import { albums } from "./albums_data.ts";
 import { useState } from 'react';
 import TierFilter from './components/TierFilter.tsx';
+import GenreFilter from './components/GenreFilter.tsx';
 
 function App() {
-  const [tier, setTier] = useState(1);
-  const filteredAlbums = tier === 0 ? albums : albums.filter((a) => a.tier === tier)
+  const [tierGroup, setTierGroup] = useState(1);
+  const [genreSelections, setGenreSelections] = useState<string[]>([]);
+
+  function handleGenreChange(genre: string, checked: boolean) {
+    setGenreSelections((currentGenres) => {
+      if (checked) {
+        return [...currentGenres, genre];
+      }
+      return currentGenres.filter((item) => item !== genre);
+    });
+  }
+
+  const filteredAlbums = albums.filter((a) => {
+    const matchesTier = tierGroup === 0 || a.tier === tierGroup;
+    const matchesGenres = 
+      genreSelections.length === 0 
+      || a.genres.some((genre) => genreSelections.includes(genre));
+
+    return matchesTier && matchesGenres;
+    });
 
   return (
-      <section id="center">
-        <TierFilter setTier={setTier} />
+      <section>
+        <TierFilter setTierGroup={setTierGroup} />
+        <GenreFilter onGenreChange={handleGenreChange}/>
         <AlbumList albums={filteredAlbums} />
       </section>
   )
