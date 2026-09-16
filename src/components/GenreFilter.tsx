f767a86abe8ba1d4f73fc7deb150bb9a538082a6
Adam Maxfield <adammaxfield@gmail.com>
2026-09-16 13:10:55 -0700

    Creates a hook for filtering. Removes filter functionality from App. Adds parent Filter component and minor design framework.

--- src/components/GenreFilter.tsx
+++ src/components/GenreFilter.tsx
@@ -1,28 +1,25 @@
-import { albums } from "../albums_data";
-  import "./GenreFilter.scss";
+import "./GenreFilter.scss";
 
-  type GenreProps = {
-    onGenreChange: (genre: string, checked: boolean) => void;
-  };
+export interface GenreFilterProps {
+  genres: string[];
+  selectedGenres: string[];
+  onGenreChange: (genre: string, checked: boolean) => void;
+}
 
-  const genreAssortment = [
-    ...new Set(albums.flatMap((album) => album.genres))
-  ]
+function GenreFilter({ genres, selectedGenres, onGenreChange }: GenreFilterProps) {
+  return (
+    <form id="genre-filter">
+      {genres.map((genre) => (
+        <label key={genre}>
+          <input
+            type="checkbox"
+            checked={selectedGenres.includes(genre)}
+            onChange={(e) => onGenreChange(genre, e.target.checked)} />
+          {genre}
+        </label>
+      ))}
+    </form>
+  );
+}
 
-  function GenreFilter({onGenreChange}: GenreProps) {
-
-    return (
-      <form id="genre-filter">
-        {genreAssortment.map((genre) => (
-          <label key={genre}>
-            <input
-              type="checkbox"
-              onChange={(e) => onGenreChange(genre, e.target.checked)}/>
-            {genre}
-          </label>
-        ))}
-      </form>
-    );
-  }
-
-  export default GenreFilter;
+export default GenreFilter;
